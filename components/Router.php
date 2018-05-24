21f220bd66a5fabd4dd6c3f81c129fe5f5dbf640
A1exMorris <wertava@gmail.com>
2018-05-24 16:45:12 +0300

    Bones done

---
+++ components/Router.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Alex
+ * Date: 24.05.2018
+ * Time: 16:42
+ */
---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Alex
+ * Date: 24.05.2018
+ * Time: 14:54
+ */
+echo 'haha!';
